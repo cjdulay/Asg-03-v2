89fe083804949575bcc335d864ed86a8c4bfb305
jmgrageda <jonamikhaela@gmail.com>
2018-03-21 21:15:28 -0600

    updated Salary

--- Salary.java
+++ Salary.java
@@ -1,3 +1,36 @@
 public class Salary extends Employee
 {
+	public double yearSal;
+
+	Salary(){
+	}
+
+	Salary(String name, String employeeNo, String department, double yearSalary){
+		super(name, employeeNo, department, 'S');
+		yearSal = yearSalary;
+	}
+
+	public void setYearSal(double yearlySal){
+		yearSal = yearlySal
+	}
+
+	public double getYearSal(){
+		return yearSal;
+	}
+
+	public double calcWeeklySalary(){
+
+	}
+
+	public boolean equals (Employee e){
+
+	}
+
+	public String toString(){
+
+	}
+
+	public void writeDate(){
+		
+	}
 }
