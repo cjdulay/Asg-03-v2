524dfc31b4e4fedd2c081bc2d22d5d208ba12140
jmgrageda <jonamikhaela@gmail.com>
2018-03-22 19:14:01 -0600

    salary

--- Salary.java
+++ Salary.java
@@ -1,64 +1,38 @@
 import java.util.*;
-
 import java.io.*;
-
 public class Salary extends Employee
-
 {
-
     public double yearSal;
 
-
     Salary() throws IOException{
-
     }
-
 
     Salary(String name, String employeeNo, String department, double yearSalary){
-
         super(name, employeeNo, department, 'S');
-
         yearSal = yearSalary;
-
     }
-
 
     public void setYearSal(double yearlySal){
-
         yearSal = yearlySal;
-
     }
-
 
     public double getYearSal(){
-
         return yearSal;
-
     }
-
 
     public double calcWeeklySalary(){
-
         double weekSal = yearSal / 52;
-
         return weekSal;
-
     }
-
-
-    /*
 
     public String toString(){
-
-
+		String line = "";
+		String ysal = "Yearly Salary: " + getYearSal() + "\n";
+		line = 	super.toString() + ySal;
+        return line;
     }
-
 
     public void writeData(){
 
-
     }
-
-     */
-
 }
