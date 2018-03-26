2fa003ea394025af9af28559463fcc4374fc44d9
cjdulay <37643523+cjdulay@users.noreply.github.com>
2018-03-25 23:08:42 -0600

    Update Hourly.java

--- Hourly.java
+++ Hourly.java
@@ -1,33 +1,29 @@
 public class Hourly extends Employee
 
 {
-
     private double hourlyRate;
-
     private double hourWeek;
+    private final double FORTY = 40;
+    private final double OVERTIME = 1.5;
 
     //Constructors
 
-    Hourly(){
-
+    public Hourly(){
+        super();
+        hourlyRate = 0.0;
+        hourWeek = 0.0;
     }
 
-    Hourly(String name, String employeeNo, String department, double hourRate, double weekHours){
-
+    public Hourly(String name, String employeeNo, String department, double hourRate, double weekHours){
         super(name, employeeNo, department, 'H');
-
         hourlyRate = hourRate;
-
         hourWeek = weekHours;
-
     }
 
     //Mutators
 
     public void setHourlyRate(double hr){
-
         hourlyRate = hr;
-
     }
 
     public void setHourWeek(double hw){
@@ -41,27 +37,17 @@ public class Hourly extends Employee
     }
 
     public double getHourWeek(){
-
         return hourWeek;
-
     }
 
     //Helper Methods
 
-    public double calcWeeklySalary(){
-
-        double wage = 0.0;
-
-        double overtime = 0.0;
-
-        double newRate = 0.0;
-
-        double add = 0.0;
-
-        final double FORTY = 40;
-
-        final double OVERTIME = 1.5;
-
+    @Override public double calcWeeklySalary(){
+        double wage;
+        double overtime;
+        double newRate;
+        double add;
+        
         if (hourWeek > FORTY){
             newRate = hourlyRate * OVERTIME;
             overtime = hourWeek - FORTY;
@@ -75,10 +61,8 @@ public class Hourly extends Employee
         return wage;
     }
 
-    public boolean equals(Employee e){
-
+    @Override public boolean equals(Employee e){
         boolean equals;
-
         if (super.getEmployeeNo() == e.getEmployeeNo()){
             equals = true;
         }
@@ -88,27 +72,20 @@ public class Hourly extends Employee
         return equals;
     }
 
-    public String toString(){
-
+    @Override public String toString(){
         String line = "";
-
         String hourRate = "Hour rate is: " + getHourlyRate() + "\n";
-        String hoursWorkedWk = "    Hours worked this week: " + getHourWeek() + "\n";
-        String top = "";
+        String hoursWorkedWk = "Hours worked this week: " + getHourWeek() + "\n";
 
-        line = super.toString() + hourRate + hoursWorkedWk + top;
+        line = super.toString() + hourRate + hoursWorkedWk;
 
         return line;
     }
 
     public String writeData(){
-
         String data = "";
-
         data = super.writeData() + " " + getHourlyRate() + " " + getHourWeek() + "\n";
-
         return data;
-
     }
 
 }
