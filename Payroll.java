6505b8f36cbf9807a65a9c65a8599d9e50f39534
cjdulay <37643523+cjdulay@users.noreply.github.com>
2018-03-25 23:09:00 -0600

    Update Payroll.java

--- Payroll.java
+++ Payroll.java
@@ -2,11 +2,12 @@ import java.util.*;
 import java.io.*;
 public class Payroll
 {
-    private ArrayList <Employee> a = new ArrayList <Employee>();
-    public void run(){
-
+    private ArrayList <Employee> employee = new ArrayList <Employee>();
+    public Payroll(String file) throws IOException{
+        readFile(file);
     }
 
+    //Reads the file
     public void readFile(String in) throws IOException{
         String name = "";
         String employeeNo = "";
@@ -31,12 +32,12 @@ public class Payroll
                 hourWage = inFile.nextDouble();
                 hoursWorked = inFile.nextDouble();                  
                 Employee h = new Hourly(name, employeeNo, department, hourWage, hoursWorked);
-                a.add(h);
+                employee.add(h);
             }
             else if (type == 'S'){
                 salary = inFile.nextDouble();
                 Employee s = new Salary(name, employeeNo, department, salary);
-                a.add(s);
+                employee.add(s);
             }
             else if (type == 'C'){
                 weeksSinceEmp = inFile.nextInt();
@@ -45,37 +46,117 @@ public class Payroll
                 yearSales = inFile.nextDouble();
                 commissionRate = inFile.nextDouble();
                 Employee c = new Commission(name, employeeNo, department, weeksSinceEmp, baseWeekSalary, yearSales, commissionRate);
-                a.add(c);
+                employee.add(c);
             }
         }
         inFile.close();
     }
 
-    public Employee checkEmpNum(String num){
-        Employee valid = null;
+    // Checks if the employee is in the array by comparing employee no
+    public int checkEmpNum(String num){
         boolean found = false;
-
         int i = 0;
-        while (i < a.size() && !found){
-            if (a.get(i).getEmployeeNo().equals(num)){
-                valid = a.get(i);
+        int counter = -1;
+        int numOfEmp = employee.size();
+        while (!found && i < numOfEmp){
+            if (num.equals(employee.get(i).getEmployeeNo())){
+                counter = i;
                 found = true;
             }
             i++;
         }
-        return valid;
+        return counter;
     }
 
-    public void empQuery(String num)
-    {
-        Employee foundItem = null;
-
-        foundItem = checkEmpNum(num);
-        if (foundItem != null)
-            System.out.println (foundItem.printDetails());
-        else
-            System.out.println ("This employee number " + num + " does not exist.");
-
+    //Methods to add employees
+    public void addHourly(String name, String employeeNo, String department, double hourWage, double hoursWorked){
+        Hourly h = new Hourly(name, employeeNo, department, hourWage, hoursWorked);
+        employee.add(h);
     }
 
+    public void addSalary(String name, String employeeNo, String department, double salary){
+        Salary s = new Salary(name, employeeNo, department, salary);
+        employee.add(s);
+    }
+
+    public void addCommission(String name, String employeeNo, String department, int weeksSinceEmp, double baseWeekSalary, double yearSales, double commissionRate){
+        Commission c = new Commission(name, employeeNo, department, weeksSinceEmp, baseWeekSalary, yearSales, commissionRate);
+        employee.add(c);
+    }
+
+    //Prints employee info
+
+    public void printEmployeeInfo(int counter){
+        System.out.print(employee.get(counter).toString());
+    }
+
+    //Delete an employee
+
+    public void removeEmployee(int counter){
+        employee.remove(counter);
+    }
+
+    //Calculates weekly salary
+    public double weeklySalary(int counter){
+        double salary = employee.get(counter).calcWeeklySalary();
+        return salary;
+    }
+
+    //Prints weekly salary report for everything in the array
+    public void printSalaryReport(){
+        System.out.print("\nWeekly Salary Report\n");
+        int numberOfEmployees;
+        String empType = "";
+        numberOfEmployees = employee.size();
+
+        for(int i = 0; i < numberOfEmployees; i++){
+            char type = employee.get(i).getType();
+            if (type == 'H'){
+                empType = "Hourly";
+            }
+            else if (type == 'S'){
+                empType = "Salary";
+            }
+            else if (type == 'C'){
+                empType = "Commission";
+            }
+            System.out.print(employee.get(i).getName() + "\t" + employee.get(i).getEmployeeNo() + "\t" + employee.get(i).getDepartment() + "\t" + empType + "\t $" + weeklySalary(i) + "\n");
+        }
+    }
+
+    public void topSellersList(){
+        int employeeSize;
+        char type;
+        String name;
+
+        System.out.print("\nTop Sellers\n");
+        employeeSize = employee.size();
+
+        for(int i = 0; i < employeeSize; i++){
+            type = employee.get(i).getType();
+            if (type == 'C'){
+                if (employee.get(i).topSeller() == true){
+                    name = employee.get(i).getName();
+                    System.out.print(name);
+                }
+                else if(employee.get(i).topSeller() == false){
+                    System.out.print("No top sellers");
+                }
+            }
+        }
+        System.out.print("\n");
+    }
+
+    public void printReport(String outFile) throws IOException{
+        PrintWriter out = new PrintWriter(outFile);
+        int employeeSize;
+
+        employeeSize = employee.size();
+
+        for (int i = 0; i < employeeSize; i++){
+            out.println(employee.get(i).writeData());
+        }
+        out.close();
+    }
 }
+
